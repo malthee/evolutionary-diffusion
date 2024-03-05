08900295a71a19b09382a115cb59c7578355ce26
Marcel Salvenmoser <18032233+malthee@users.noreply.github.com>
2024-03-05 19:50:30 +0100

    Try to fix description error evolutionary package

--- evolutionary/setup.py
+++ evolutionary/setup.py
@@ -4,13 +4,11 @@ setup(
     name='evolutionary',
     version='0.1.2',
     author='malthee',
-    description='Base package defining a framework for evolutionary algorithms to be used with generative networks. '
-                'Splits up the Solution-Representation into arguments and results.',
-    long_description='This package includes generic classes for evolutionary computation in a generational environment.'
-                     ' Crossover and Mutation happens on the argument (A) level, whilst the fitness is evaluated on '
-                     'the result (R) level. '
-                     'SolutionCandidates are created by a SolutionCreator, their representation is split into '
-                     'arguments (A) and result (R). ',
+    description='''Base package defining a framework for evolutionary algorithms to be used with generative networks.
+                   Splits up the Solution-Representation into arguments and results.''',
+    long_description='''This package includes generic classes for evolutionary computation in a generational environment.
+                     Crossover and Mutation happens on the argument (A) level, whilst the fitness is evaluated on the result (R) level.
+                     SolutionCandidates are created by a SolutionCreator, their representation is split into arguments (A) and result (R).''',
     packages=find_packages(),
     install_requires=[
         'matplotlib',
