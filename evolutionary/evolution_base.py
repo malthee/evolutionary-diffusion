3262a693fbc5fba4be1bc40ca859f2ac2f3dd683
Marcel Salvenmoser <18032233+malthee@users.noreply.github.com>
2024-03-26 10:02:05 +0100

    More evaluators, Topology for IslandModel, Visualizations

--- evolutionary/evolution_base.py
+++ evolutionary/evolution_base.py
@@ -6,6 +6,7 @@ The fitness type is generic and can be either a single value or a sequence of va
 optimization.
 By default, uses maximization of fitness values, to minimize a fitness value, use the negative value.
 """
+from math import log
 from typing import Generic, TypeVar, Optional, List, Callable, Sequence, Any
 from abc import abstractmethod, ABC
 from tqdm import tqdm
@@ -65,6 +66,63 @@ class InverseEvaluator(SingleObjectiveEvaluator):
 
     def evaluate(self, result: R_covariant) -> SingleObjectiveFitness:
         return -self._evaluator.evaluate(result)
+
+
+class CappedEvaluator(SingleObjectiveEvaluator):
+    """
+    Caps the fitness value of a single-objective evaluator to a maximum value.
+    Useful for limiting the fitness value to a certain threshold.
+    """
+
+    def __init__(self, evaluator: SingleObjectiveEvaluator[R_covariant], cap_value: float):
+        """
+        Initializes the capped evaluator with a base evaluator and a cap value.
+
+        Args:
+            evaluator: The base evaluator providing initial fitness values.
+            cap_value: The maximum fitness value allowed. If the base evaluator returns a value greater than this,
+                       the capped value will be used instead.
+        """
+        self._evaluator = evaluator
+        self._cap_value = cap_value
+
+    def evaluate(self, result: R_covariant) -> SingleObjectiveFitness:
+        original_fitness = self._evaluator.evaluate(result)
+        return min(original_fitness, self._cap_value)
+
+
+class GoalDiminishingEvaluator(SingleObjectiveEvaluator):
+    """
+    Modifies fitness values from an underlying evaluator based on a goal value.
+    For fitness values exceeding the goal, a logarithmic diminishing returns effect is applied.
+    """
+
+    def __init__(self, evaluator: SingleObjectiveEvaluator[R_covariant], goal_value: SingleObjectiveFitness,
+                 diminish_scale: float = 1.0):
+        """
+        Initializes the evaluator with a goal value and a scale for diminishing returns.
+
+        Args:
+            evaluator: The base evaluator providing initial fitness values.
+            goal_value: The fitness threshold beyond which diminishing returns are applied.
+            diminish_scale: Adjusts the rate of diminishing returns for values above the goal. The lower, the stronger
+                            the diminishing effect. Default is 1.0.
+        """
+        assert diminish_scale > 0, "Diminish scale must be greater than 0."
+
+        self._evaluator = evaluator
+        self._goal_value = goal_value
+        self._diminish_scale = diminish_scale
+
+    def evaluate(self, result: R_covariant) -> SingleObjectiveFitness:
+        original_fitness = self._evaluator.evaluate(result)
+
+        # Apply the diminishing returns logic only for values exceeding the goal
+        if original_fitness <= self._goal_value:
+            return original_fitness
+        else:
+            # Apply diminishing returns to the portion of the fitness above the goal.
+            return self._goal_value + log(1 + (original_fitness - self._goal_value) * self._diminish_scale)
 
 
 class Mutator(Generic[A], ABC):
